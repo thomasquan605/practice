f4a7dabca132892d06af11b6f2222bcc070d92de
thomasquan605 <thomas.quan605@gmail.com>
2025-07-10 21:41:07 +0800

    回弹动画

--- day2/dragball.js
+++ day2/dragball.js
@@ -1,3 +1,58 @@
+let lasttime = 0
+const nextFrame = window.requestAnimationFrame || 
+                  window.webkitRequestAnimationFrame || 
+                  window.mozRequestAnimationFrame || 
+                  window.msRequestAnimationFrame ||
+                  function(callback) {
+                    let curtime = +new Date, delay = Math.max(1000 / 60, 1000 / 60 - (curtime - lasttime));
+                    lasttime = curtime + delay
+                    return setTimeout(callback, delay)
+                  };
+
+// 偷个懒                  
+const cancelFrame = window.cancelAnimationFrame ||
+                    window.webkitCancelAnimationFrame ||
+                    clearTimeout;
+
+
+// 动画函数
+const tween = {
+  linear: function (t, b, c, d) { return c * t / d + b; },
+  ease: function (t, b, c, d) { return -c * ((t = t / d - 1) * t * t * t - 1) + b; },
+  'ease-in': function (t, b, c, d) { return c * (t /= d) * t * t + b; },
+  'ease-out': function (t, b, c, d) { return c * ((t = t / d - 1) * t * t + 1) + b; },
+  'ease-in-out': function (t, b, c, d) { if ((t /= d / 2) < 1) return c / 2 * t * t * t + b; return c / 2 * ((t -= 2) * t * t + 2) + b; },
+  bounce: function (t, b, c, d) { if ((t /= d) < (1 / 2.75)) { return c * (7.5625 * t * t) + b; } else if (t < (2 / 2.75)) { return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b; } else if (t < (2.5 / 2.75)) { return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b; } else { return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b; } }
+}
+
+
+
+// target 为目标距离
+function slideTo(el, target) {
+
+  // 动画持续时间                    
+  const duration = 1000;
+  let timer = null
+
+  const stime = Date.now()
+  cancelFrame(timer)
+  ani()
+  function ani() {
+    const offset = Math.min(duration, Date.now() - stime)
+    const s = tween.ease(offset, 0, 1, duration)
+    if (offset < duration) {
+      el.style.transform = `translate(0px, ${(1 - s) * target}px)`
+      timer = nextFrame(ani)
+    }
+  }
+}
+
+// 对象，属性，行为
+// 行为应该在外部调用
+// 封装，思考属性，行为？
+
+// 思考下拉刷新的过程中
+
 class DragBall {
   state = {
     x: 0,
@@ -44,9 +99,9 @@ class DragBall {
     this.state.rate = 1
     this.state.lastY = 0
 
-    setTimeout(() => {
-      this.drayMove(0)
-    }, 1500);
+      // this.drayMove(0)
+      const rect = this.draggerEle.getBoundingClientRect()
+      slideTo(this.draggerEle, rect.top)
   }
 
   move(touch) {
@@ -88,6 +143,7 @@ class DragBall {
   }
 
   touchEnd(e) {
+    console.log('touchEnd')
     this.deinit()
   }
 
